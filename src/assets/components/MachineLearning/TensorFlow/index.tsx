85a5d5aeaf24d8bd3455d9dd49de378b3bb46f72
Ed Brown <e_brown18@hotmail.co.uk>
2023-12-13 14:47:07 +0000

    tensorflow example enhancements

--- src/assets/components/MachineLearning/TensorFlow/index.tsx
+++ src/assets/components/MachineLearning/TensorFlow/index.tsx
@@ -1,15 +1,34 @@
 import * as tf from '@tensorflow/tfjs'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { coordsType, pathType, sleep } from '~/assets/components/MachineLearning/Bellman/functions'
 import { GridUi } from '~/assets/components/MachineLearning/components/GridUi'
 
 class DQNAgent {
+	// main learning properties
 	private model: tf.Sequential
 	private readonly actions: number[]
 	private readonly learningRate: number
 	private readonly discountFactor: number
 	private readonly stateSize: number
 
+	// epsilon-greedy properties
+	private epsilon: number = 1.0
+	private epsilonMin: number = 0.01
+	private epsilonDecay: number = 0.995
+
+	// aggregate data for reviewing learning quality
+	private timesTrained: number = 0
+	private batchSize: number = 10
+	private thisBatch: number[] = []
+	private avStepsPerTrainingBatch: number[] = []
+
+	/**
+	 *
+	 * @param actions quantity of available actions. in this example, up/down/left/right = [0, 1, 2, 3]
+	 * @param stateSize number of states. in this example 2 (x,y)
+	 * @param learningRate learningRate
+	 * @param discountFactor discountFactor
+	 */
 	constructor(actions: number[], stateSize: number, learningRate: number = 0.01, discountFactor: number = 0.95) {
 		this.actions = actions
 		this.stateSize = stateSize
@@ -25,6 +44,25 @@ class DQNAgent {
 		model.add(tf.layers.dense({ units: this.actions.length, activation: `linear` }))
 		model.compile({ optimizer: tf.train.adam(this.learningRate), loss: `meanSquaredError` })
 		return model
+	}
+
+	public updateAggregateData(numSteps : number) {
+		this.timesTrained++
+
+		if ((this.timesTrained % this.batchSize) === 0) {
+			const sum = this.thisBatch.reduce((a, b) => a + b, 0)
+			const avg = (sum / this.thisBatch.length) || 0
+
+			this.thisBatch = [] // reset
+
+			this.avStepsPerTrainingBatch.push(avg)
+		}
+
+		this.thisBatch.push(numSteps)
+	}
+
+	public getAggregates() {
+		return this.avStepsPerTrainingBatch
 	}
 
 	public async train(state: number[], actionIndex: number, reward: number, nextState: number[], done: boolean): Promise<void> {
@@ -56,13 +94,27 @@ class DQNAgent {
 		nextQ.dispose()
 	}
 
+	/** Uses epsilon-greedy exploration strategy */
 	public chooseAction(state: number[]): number {
-		const stateTensor = tf.tensor2d([state])
-		const qValues = this.model.predict(stateTensor) as tf.Tensor
-		const actionIndex = qValues.argMax(-1).arraySync() as number
-		stateTensor.dispose()
-		qValues.dispose()
-		return this.actions[actionIndex]
+		if (Math.random() < this.epsilon) {
+			return Math.floor(Math.random() * this.actions.length)
+		} else {
+			const stateTensor = tf.tensor2d([state])
+			const qValues = this.model.predict(stateTensor) as tf.Tensor
+			const actionIndex = qValues.argMax(-1).arraySync() as number
+			stateTensor.dispose()
+			qValues.dispose()
+			return this.actions[actionIndex]
+		}
+	}
+
+	/**
+	 * Decay the epsilon value so there is less randomness the longer it learns
+	 */
+	public updateEpsilon(): void {
+		if (this.epsilon > this.epsilonMin) {
+			this.epsilon *= this.epsilonDecay
+		}
 	}
 }
 
@@ -70,9 +122,11 @@ class GridEnvironment {
 	private grid: number[][]
 	private gridSize: number
 	private agentPosition: {x: number, y: number}
+	private obstacles
 
-	constructor(size: number) {
+	constructor(size: number, obstacles) {
 		this.gridSize = size
+		this.obstacles = obstacles
 		this.grid = Array.from({ length: size }, () => Array(size).fill(0))
 		this.reset()
 	}
@@ -112,9 +166,19 @@ class GridEnvironment {
 		if (this.agentPosition.x === this.gridSize - 1 && this.agentPosition.y === this.gridSize - 1) {
 			reward = 1
 			done = true
+		} else if (this.obstacles.some(obstacle => obstacle.row === this.agentPosition.x && obstacle.col === this.agentPosition.y)) {
+			reward = -((this.gridSize - 1) * 2)
+		} else {
+			reward = this.getDistanceLivingCost()
 		}
 
 		return { state: this.getState(), reward, done }
+	}
+
+	private getDistanceLivingCost() {
+		let maxDistance = this.gridSize - 1
+
+		return -((maxDistance * 2) - this.agentPosition.x - this.agentPosition.y) / 100
 	}
 
 	private getState(): number[] {
@@ -124,75 +188,181 @@ class GridEnvironment {
 	}
 }
 
-async function trainAgent(episodes: number): Promise<void> {
-	const env = new GridEnvironment(3) // 5x5 grid
-	const agent = new DQNAgent([0, 1, 2, 3], 2) // Actions: up, down, left, right
+async function trainAgent(agent : DQNAgent, gridSize : number, obstacles, episodes: number): Promise<pathType> {
+	const env = new GridEnvironment(gridSize, obstacles) // 5x5 grid
+
+	let stepsTaken : pathType = null
 
 	for (let episode = 0; episode < episodes; episode++) {
 		let state = env.reset()
 		let done = false
 		let totalReward = 0
-		const maxIteration = 1000
+		const maxIteration = 100000000
 		let iteration = 0
+
+		stepsTaken = {
+			path: [],
+			complete: false,
+		}
 
 		while (!done && iteration < maxIteration) {
 			const action = agent.chooseAction(state)
+			const originalState = state
 			const { state: nextState, reward, done: isDone } = env.step(action)
 
-			console.log(`training ${iteration}`)
-			await agent.train(state, action, reward, nextState, isDone)
-			console.log(`training end ${iteration}`)
+			// console.log(`training ${iteration}`)
+			await agent.train(originalState, action, reward, nextState, isDone)
+			// console.log(`training end ${iteration}`)
+
+			if (true) {
+			// only degrade epsilon under a circumstance??
+				agent.updateEpsilon()
+			}
 
 			state = nextState
 			done = isDone
 			totalReward += reward
+
+			stepsTaken.path.push({
+				row: nextState[0],
+				col: nextState[1],
+			})
+
+			if (done) {
+				stepsTaken.complete = true
+				agent.updateAggregateData(stepsTaken.path.length)
+			}
 
 			iteration++
 		}
 
 		console.log(`Episode ${episode}, Total Reward: ${totalReward}`)
 	}
+
+	return stepsTaken
 }
 
-export function TensorFlow() {
+function TensorFlow({ gridSize, obstacles, numTrainings }) {
 	const [isLearning, setIsLearning] = useState(false)
 	const [isStarted, setIsStarted] = useState(false)
 	const [preferredPath, setPreferredPath] = useState(null)
 	const [isCompletePath, setIsCompletePath] = useState(false)
-	const [localWinState, setLocalWinState] = useState<coordsType>({ row: -1, col: -1 })
-	const [localStartState, setLocalStartState] = useState<coordsType>({ row: -1, col: -1 })
+	const [localWinState, setLocalWinState] = useState<coordsType>({ row: gridSize - 1, col: gridSize - 1 })
+	const [localStartState, setLocalStartState] = useState<coordsType>({ row: 0, col: 0 })
+	const [notes, setNotes] = useState(``)
+	const ai = useRef(new DQNAgent([0, 1, 2, 3], 2)) // Actions: up, down, left, right, stateSize: 2 (i.e. 2 states: flat grid. x,y coords))
 
 	useEffect(() => {
 		if (!isLearning) {
 			return
 		}
 
-		// Train for 100 episodes
-		trainAgent(1).then(() => {
-			setLocalStartState(localWinState)
+		async function trainAgentBulk(numTrainings) : Promise<pathType> {
+			let data
+			const arr = Array.from({ length: numTrainings }, (value, index) => index)
+
+			setNotes(`Started training`)
+
+			for (const iterator of arr) {
+				data = await trainAgent(ai.current, gridSize, obstacles, 1)
+				setNotes(`Completed training ${iterator + 1}/${numTrainings}`)
+			}
+			setNotes(`Training finished`)
+			return data
+		}
+
+		// Train for n episodes
+		trainAgentBulk(numTrainings).then((data) => {
+			setLocalStartState(localStartState)
 			setLocalWinState(localWinState)
 
-			const getPreferredPath = () : pathType => { return { path: [], complete: false } }
-			const { path, complete } = getPreferredPath()
+			const { path, complete } = data
 
 			setPreferredPath(path)
 			sleep(500).then(() => setIsLearning(false))
 			setIsCompletePath(complete)
+
+			console.log(ai.current.getAggregates())
 		})
 	}, [isLearning])
-	return <GridUi preferredPath={preferredPath}
+
+	return <GridUi
+		gridSize={gridSize}
+		obstacles={obstacles}
+		preferredPath={preferredPath}
 		isStarted={isStarted}
 		setIsStarted={setIsStarted}
 		isLearning={isLearning}
 		setIsLearning={setIsLearning}
 		isCompletePath={isCompletePath}
 		startState={localStartState}
-		winState={localWinState}/>
+		winState={localWinState}
+		notes={notes}/>
 }
 
-// // Example usage
-// const actions = [0, 1, 2, 3] // Define actions (e.g., up, down, left, right)
-// const agent = new DQNAgent(actions)
-// // Train the agent with state, action, reward, nextState, and done signal
-// // agent.train(...)
-// // Use agent.chooseAction(state) to navigate
+export function TensorFlowSimple() {
+	const gridSize = 5
+	const numTrainings = 100
+
+	const obstacles = []
+
+	return <TensorFlow gridSize={gridSize} obstacles={obstacles} numTrainings={numTrainings}/>
+}
+
+export function TensorFlowObstacles() {
+	const gridSize = 5
+	const numTrainings = 100
+	const obstacles = [
+		// { row: 0, col: 0 },
+		// { row: 1, col: 0 },
+		// { row: 2, col: 0 },
+		{ row: 3, col: 0 },
+		{ row: 4, col: 0 },
+		{ row: 5, col: 0 },
+		{ row: 6, col: 0 },
+		// { row: 1, col: 2 },
+		// { row: 2, col: 2 },
+		// { row: 3, col: 2 },
+		// { row: 4, col: 2 },
+		{ row: 5, col: 2 },
+		{ row: 5, col: 3 },
+		{ row: 5, col: 4 },
+		{ row: 5, col: 5 },
+		// { row: 5, col: 6 },
+		{ row: 5, col: 7 },
+		{ row: 0, col: 4 },
+		// { row: 0, col: 6 },
+		// { row: 1, col: 6 },
+	]
+
+	return <TensorFlow gridSize={gridSize} obstacles={obstacles} numTrainings={numTrainings}/>
+}
+
+export function TensorFlowObstaclesMedium() {
+	const gridSize = 8
+	const numTrainings = 100
+	const obstacles = [
+		// { row: 0, col: 0 },
+		// { row: 1, col: 0 },
+		{ row: 2, col: 0 },
+		{ row: 3, col: 0 },
+		{ row: 4, col: 0 },
+		{ row: 5, col: 0 },
+		{ row: 6, col: 0 },
+		{ row: 1, col: 2 },
+		{ row: 2, col: 2 },
+		{ row: 3, col: 2 },
+		{ row: 4, col: 2 },
+		{ row: 5, col: 2 },
+		{ row: 5, col: 3 },
+		{ row: 5, col: 4 },
+		{ row: 5, col: 5 },
+		{ row: 5, col: 6 },
+		{ row: 5, col: 7 },
+		{ row: 0, col: 4 },
+		{ row: 0, col: 6 },
+		{ row: 1, col: 6 },
+	]
+
+	return <TensorFlow gridSize={gridSize} obstacles={obstacles} numTrainings={numTrainings}/>
+}
